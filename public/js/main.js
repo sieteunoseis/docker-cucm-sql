703104d2b1e9892e862e11cf9597ca2e3dfde895
Jeremy <jeremy.worden@gmail.com>
2023-04-06 21:33:45 -0700

    v0.2.5

--- public/js/main.js
+++ public/js/main.js
@@ -33,6 +33,16 @@ document.addEventListener(
     $('[data-toggle="tooltip"]').tooltip({
       trigger: "hover",
     });
+
+    // Dark mode light switch
+    $("input[id='lightSwitch']").on("change", function() {
+      if ($("html").attr("data-bs-theme") == 'light') {
+        $("html").attr("data-bs-theme", "dark");
+      } else if ($("html").attr("data-bs-theme") == "dark") {
+        $("html").attr("data-bs-theme", "light");
+      }
+    });
+
   },
   false
 );
