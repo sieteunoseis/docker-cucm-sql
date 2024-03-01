3ac2f832a165d6578efe570d1ccd83fe236f78e9
Jeremy <jeremy.worden@gmail.com>
2024-03-01 14:22:51 -0800

    Removed a console log

--- public/js/main.js
+++ public/js/main.js
@@ -170,7 +170,6 @@ async function runQuery() {
       showAlert("Error", "No results returned.");
     } else {
       obj = await res.json();
-      console.log(obj);
       // 500 error, else 200 OK
       if (obj?.faultcode || obj?.code === "ECONNRESET") {
         var fault = obj?.faultstring || obj?.code;
